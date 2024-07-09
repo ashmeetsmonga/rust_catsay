7f446a5a27c879578ac6b88a0d0aa81a06f094c0
ashmeetsmonga <ashmeetsmonga@gmail.com>
2024-07-09 23:42:10 +0530

    Second version

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-fn main() {
-    let message = std::env::args().nth(1)
-    .expect("Missing the message. Usage: cat_say <message>");
+use clap::Parser;
+use colored::Colorize;
 
-    println!("{}", message);
+#[derive(Parser)]
+struct Options {
+    #[clap(default_value = "Meow!")]
+    message: String,
+
+    #[clap(short = 'd', long = "dead")]
+    ///Make the cat appear dead
+    dead: bool,
+}
+
+fn main() {
+    let options = Options::parse();
+    let message = options.message;
+    let eye = if options.dead {"x"} else {"o"};
+
+    println!("{}", message.bright_yellow().underline().on_purple());
     println!(" \\");
     println!("  \\");
     println!("   /\\_/\\");
-    println!("  ( o o )");
+    println!("  ( {eye} {eye} )", eye=eye.red().bold());
     println!("  =( I )=")
 }
